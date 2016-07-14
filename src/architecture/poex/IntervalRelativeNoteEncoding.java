d6b6145385814c8f9520da60b79645365b85d88f
hexahedria <hexahedria@gmail.com>
2016-07-14 13:23:55 -0700

    Fix magic numbers in note encodings
    There were some magic numbers in note encodings specifying length of initial
    input. These were not correct and were causing incorrect behavior between
    Theano and VectorZ. Fix to use correct activation width.

--- src/architecture/poex/IntervalRelativeNoteEncoding.java
+++ src/architecture/poex/IntervalRelativeNoteEncoding.java
@@ -31,7 +31,7 @@ public class IntervalRelativeNoteEncoding implements RelativeNoteEncoding {
     @Override
     public AVector reset() {
         this.relpos = this.low_bound + this.random.nextInt(this.high_bound-this.low_bound);
-        return NNUtilities.onehot(0, 14);
+        return NNUtilities.onehot(0, this.activation_width());
     }
 
     @Override
